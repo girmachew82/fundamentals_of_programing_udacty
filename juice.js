aefa1f01283a7925d74e8b6cc802470426c5f9c1
Girmachew Gulint Tilahun <58398921+girmachew82@users.noreply.github.com>
2025-01-27 08:30:59 +0000

    Juice

---
+++ juice.js
@@ -0,0 +1,9 @@
+let t = 99;
+while(t > 0){
+    if (t == 1)
+        console.log(t +" bottle of juice on the wall!"+t+" " + " bottle of juice! Take one down, pass it around... "+ (t -1)+" bottles of juice on the wall!");
+    else    
+    console.log(t +" "+ "bottles"+" of juice on the wall!"+t+" " +  " bootles" +" of juice! Take one down, pass it around... "+ (t -1)+" bottles of juice on the wall!");
+
+    t = t-1;
+}
